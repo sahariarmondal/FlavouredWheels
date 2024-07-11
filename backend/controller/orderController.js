f6f637c0c9ca29ac0d7157f48f570111aa9b585b
sahariarmondal <148338207+sahariarmondal@users.noreply.github.com>
2024-07-12 00:13:22 +0530

    Update orderController.js
    Frontend url is updated

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //Placing user Order from frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://flavouredwheels-frontend.onrender.com";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
